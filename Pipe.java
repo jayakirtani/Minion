966442c1b374847f0544730757e5d93cb5a4e1e1
Jaya Kirtani <jaya@Rams-MacBook-Pro.local>
2015-11-26 15:09:48 -0800

    Updated Pipe.java and Platform.java files to set image in code

--- Pipe.java
+++ Pipe.java
@@ -12,6 +12,13 @@ public class Pipe extends Actor
      * Act - do whatever the Pipe wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Pipe()
+    {
+     GreenfootImage pipe = new GreenfootImage("Pipe.png");
+        setImage (pipe);
+    }
+    
+    
     public void act() 
     {
         // Add your action code here.
--- Platform.java
+++ Platform.java
@@ -12,6 +12,13 @@ public class Platform extends Actor
      * Act - do whatever the Platform wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Platform()
+    {
+     GreenfootImage platform = new GreenfootImage("Platform.png");
+        setImage (platform);
+    }
+    
+    
     public void act() 
     {
         // Add your action code here.
